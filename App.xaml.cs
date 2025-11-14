79ef2b63a2b961e64c9f22e31b883cb652f204bd
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2025-11-13 19:38:13 -0700

    WPF project structure update
    Changed project structure to have the recommended .NET Framework WPF build.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CognitiveCycleTimer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace CognitiveCycleTimer;
+
+public partial class MainWindow : Window
+{
+    private DonutAnimator? _animator;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += MainWindow_Loaded;
+        Unloaded += MainWindow_Unloaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Adjust width/height as needed to fit your TextBlock size
+        _animator = new DonutAnimator(DonutText, width: 40, height: 24, fps: 30);
+        _animator.Start();
+    }
+
+    private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+    {
+        _animator?.Stop();
+    }
+}
